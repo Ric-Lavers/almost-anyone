08ff92661065c16036dd6a88aa69dfb7f1da5c02
Ric-Lavers <ric_lavers@outlook.com>
2017-11-09 12:56:59 +1100

    removed send mail on registration controller

--- app/controllers/concerns/registrations_controller.rb
+++ app/controllers/concerns/registrations_controller.rb
@@ -3,8 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
 
 
   def after_sign_up_path_for(resource)
-    MailgunMailer.new_user_notification(current_user).deliver_now
-    format.html{redirect_to root_path}
+    # MailgunMailer.new_user_notification(current_user).deliver_now
+    # format.html{redirect_to root_path}
     new_profile_url
   end
 
